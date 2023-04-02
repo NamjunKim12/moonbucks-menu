eb0af23784296023bd368ed12a6d3c07ccf35c72
1 <wrasf175@naver.com>
2023-04-03 02:02:39 +0900

    메뉴명 수정시 서버에 PUT 요청

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,7 @@ import store from "./store/index.js";
 // - [x] 웹 서버를 띄운다.
 // - [x] 서버에 메뉴가 추가될 수 있도록 요청한다.
 // - [x] 서버에 카테고리 별 메뉴리스트를 불러온다.
-// - [] 서버에 메뉴가 수정 될 수 있도록 요청한다.
+// - [x] 서버에 메뉴가 수정 될 수 있도록 요청한다.
 // - [] 서버에 메뉴의 품절상태를 변경할 수 있도록 요청한다.
 // - [] 서버에 메뉴가 삭제 될 수 있도록 요청한다.
 
@@ -37,6 +37,23 @@ const MenuApi = {
       console.error("에러가 발생했습니다.");
     }
   },
+
+  async updateMenu(category, menuId, name) {
+    const response = await fetch(
+      `${BASE_URL}/category/${category}/menu/${menuId}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name }),
+      }
+    );
+    if (!response.ok) {
+      console.error("에러가 발생했습니다.");
+    }
+    return response.json();
+  },
 };
 
 function App() {
@@ -59,9 +76,11 @@ function App() {
 
   const render = () => {
     const template = this.menu[this.currentCategory]
-      .map((item, index) => {
+      .map((item) => {
         return `
-      <li data-menu-id='${index}' class="menu-list-item d-flex items-center py-2">
+      <li data-menu-id='${
+        item.id
+      }' class="menu-list-item d-flex items-center py-2">
         <span class="w-100 pl-2 menu-name ${item.soldOut ? "sold-out" : ""}">${
           item.name
         }</span>
@@ -109,12 +128,14 @@ function App() {
     render();
     $("#espresso-menu-name").value = "";
   };
-  const updateMenuName = (e) => {
+  const updateMenuName = async (e) => {
     const menuId = e.target.closest("li").dataset.menuId;
     const $menuName = e.target.closest("li").querySelector(".menu-name");
     const updatedMenuName = prompt("메뉴명을 수정하세요", $menuName.innerText);
-    this.menu[this.currentCategory][menuId].name = updatedMenuName;
-    store.setLocalStorage(this.menu);
+    await MenuApi.updateMenu(this.currentCategory, menuId, updatedMenuName);
+    this.menu[this.currentCategory] = await MenuApi.getAllMenuByCategory(
+      this.currentCategory
+    );
     render();
   };
   const removeMenuName = (e) => {
