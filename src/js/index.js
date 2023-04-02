3a9524dc24e2ac08e52c9f112ce9457cf4b250a9
1 <wrasf175@naver.com>
2023-04-03 02:10:32 +0900

    메뉴 삭제, 품절 표시 기능 구현

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,7 @@ import store from "./store/index.js";
 // - [x] 서버에 메뉴가 추가될 수 있도록 요청한다.
 // - [x] 서버에 카테고리 별 메뉴리스트를 불러온다.
 // - [x] 서버에 메뉴가 수정 될 수 있도록 요청한다.
-// - [] 서버에 메뉴의 품절상태를 변경할 수 있도록 요청한다.
+// - [x] 서버에 메뉴의 품절상태를 변경할 수 있도록 요청한다.
 // - [] 서버에 메뉴가 삭제 될 수 있도록 요청한다.
 
 // TODO 리팩토링 부분
@@ -54,6 +54,31 @@ const MenuApi = {
     }
     return response.json();
   },
+
+  async toggleSoldOutMenu(category, menuId) {
+    const response = await fetch(
+      `${BASE_URL}/category/${category}/menu/${menuId}/soldout`,
+      {
+        method: "PUT",
+      }
+    );
+    if (!response.ok) {
+      console.error("에러가 발생했습니다.");
+    }
+    return response.json();
+  },
+
+  async deleteMenu(category, menuId) {
+    const response = await fetch(
+      `${BASE_URL}/category/${category}/menu/${menuId}`,
+      {
+        method: "DELETE",
+      }
+    );
+    if (!response.ok) {
+      console.error("에러가 발생했습니다.");
+    }
+  },
 };
 
 function App() {
@@ -81,9 +106,9 @@ function App() {
       <li data-menu-id='${
         item.id
       }' class="menu-list-item d-flex items-center py-2">
-        <span class="w-100 pl-2 menu-name ${item.soldOut ? "sold-out" : ""}">${
-          item.name
-        }</span>
+        <span class="w-100 pl-2 menu-name ${
+          item.isSoldOut ? "sold-out" : ""
+        }">${item.name}</span>
         <button
             type="button"
             class="bg-gray-50 text-gray-500 text-sm mr-1 menu-sold-out-button"
@@ -138,20 +163,25 @@ function App() {
     );
     render();
   };
-  const removeMenuName = (e) => {
+  const removeMenuName = async (e) => {
     if (confirm("정말 삭제하시겠습니까?")) {
       const menuId = e.target.closest("li").dataset.menuId;
-      this.menu[this.currentCategory].splice(menuId, 1);
-      store.setLocalStorage(this.menu);
+      await MenuApi.deleteMenu(this.currentCategory, menuId);
+      this.menu[this.currentCategory] = await MenuApi.getAllMenuByCategory(
+        this.currentCategory
+      );
       render();
     }
   };
 
-  const soldOutMenu = (e) => {
+  const soldOutMenu = async (e) => {
     const menuId = e.target.closest("li").dataset.menuId;
-    this.menu[this.currentCategory][menuId].soldOut =
-      !this.menu[this.currentCategory][menuId].soldOut;
-    store.setLocalStorage(this.menu);
+
+    await MenuApi.toggleSoldOutMenu(this.currentCategory, menuId);
+    this.menu[this.currentCategory] = await MenuApi.getAllMenuByCategory(
+      this.currentCategory
+    );
+
     render();
   };
 
